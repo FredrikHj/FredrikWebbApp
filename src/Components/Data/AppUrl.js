61cec3c1260f91ccf9d26fabed56c4f2a3e46b26
Your Name <you@example.com>
2020-06-24 10:58:45 +0200

    Fixing refresh fault

--- src/Components/Data/AppUrl.js
+++ src/Components/Data/AppUrl.js
@@ -2,11 +2,15 @@ import { navRouteName } from'./NavRouteName';
 
 import { from } from "rxjs";
 export const runAppUrl = () => {
-    let getUrlAdress = '';
+    let getAppPathAddress = '';
+    let getAppDomain = '';
+
     //Get you a slash indentifying the route as = /
-    getUrlAdress = window.location.pathname.split('')[0];
-    console.log(getUrlAdress);
+    getAppPathAddress = window.location.pathname.split('')[0];
+    getAppDomain = window.location.hostname;
+    if(getAppDomain !== 'localhost') getAppPathAddress = `FredrikDev/${navRouteName.headLine0.route}`;
+    console.log(getAppDomain);
     
-    if(getUrlAdress === '/') getUrlAdress = navRouteName.headLine0.route;
-    return getUrlAdress;
+    if(getAppPathAddress === '/') getAppPathAddress = navRouteName.headLine0.route;
+    return getAppPathAddress;
 }
